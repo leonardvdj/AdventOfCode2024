be86fa942b7e50ab91fc90f3ab5713a998619aa5
leonardvdj <leonardvdj@gmail.com>
2024-12-01 20:38:35 +0100

    Day 01 Pretty simple solves.

---
+++ Day01a/Program.cs
@@ -0,0 +1,12 @@
+﻿var input = File.ReadAllLines("input.txt")
+    .Select(l => l.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList()).ToList();
+
+var leftIds = input.Select(ids => ids[0]).Order().ToList();
+var rightIds = input.Select(ids => ids[1]).Order().ToList();
+
+var totalDistance = 0;
+
+for (var i = 0; i < input.Count; i++)
+    totalDistance += Math.Abs(leftIds[i] - rightIds[i]);
+
+Console.WriteLine(totalDistance);
---
+++ Day01b/Program.cs
@@ -0,0 +1,12 @@
+﻿var input = File.ReadAllLines("input.txt")
+    .Select(l => l.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList()).ToList();
+
+var leftIdSimilarityScores = input.Select(ids => ids[0]).ToDictionary(id => id, _ => 0);
+var rightIdCounts = input.Select(ids => ids[1]).GroupBy(id => id).ToDictionary(g => g.Key, g => g.Count());
+
+foreach (var (id, _) in leftIdSimilarityScores)
+    leftIdSimilarityScores[id] = rightIdCounts.GetValueOrDefault(id, 0);
+
+var similarityScore = leftIdSimilarityScores.Sum(kvp => kvp.Key * kvp.Value);
+
+Console.WriteLine(similarityScore);
