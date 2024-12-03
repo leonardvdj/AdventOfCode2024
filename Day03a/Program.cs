9915fdf8fcf950379867043f4d9615eccda90bd1
leonardvdj <leonardvdj@gmail.com>
2024-12-03 21:21:59 +0100

    Day 03

---
+++ Day03a/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.RegularExpressions;
+
+var input = string.Join(", ", File.ReadAllLines("input.txt"));
+
+var sum = Regex.Matches(input, @"mul\((\d+),(\d+)\)").Select(m =>
+{
+    var nums = m.Groups.Values.Skip(1).Select(g => int.Parse(g.Value)).ToList();
+    return nums[0] * nums[1];
+}).Sum();
+
+Console.WriteLine(sum);
---
+++ Day03b/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.RegularExpressions;
+
+var input = string.Join(", ", File.ReadAllLines("input.txt"));
+
+var matches = Regex.Matches(input, @"(?:mul\((\d+),(\d+)\))|(?:do(?:n't)?\(\))");
+
+var sum = 0;
+var process = true;
+foreach (Match match in matches)
+    switch (match.Value)
+    {
+        case "do()":
+            process = true;
+            break;
+        case "don't()":
+            process = false;
+            break;
+        default:
+            if (process)
+            {
+                var nums = match.Groups.Values.Skip(1).Select(g => int.Parse(g.Value)).ToList();
+                sum += nums[0] * nums[1];
+            }
+
+            break;
+    }
+
+Console.WriteLine(sum);
