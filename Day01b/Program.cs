6d2d774191e6b9cff4c83d6a755c70c6b8033c90
leonardvdj <leonardvdj@gmail.com>
2024-12-04 20:45:38 +0100

    Day 01: Fix inserting duplicate keys in dictionary, copy example.txt to build output

--- Day01b/Program.cs
+++ Day01b/Program.cs
@@ -7,7 +7,9 @@ public static class Program
         var input = File.ReadAllLines("input.txt")
             .Select(l => l.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList()).ToList();
 
-        var leftIdSimilarityScores = input.Select(ids => ids[0]).ToDictionary(id => id, _ => 0);
+        var leftIdSimilarityScores = input
+            .Select(ids => ids[0])
+            .GroupBy(id => id).ToDictionary(id => id.Key, _ => 0);
         var rightIdCounts = input.Select(ids => ids[1]).GroupBy(id => id).ToDictionary(g => g.Key, g => g.Count());
 
         foreach (var (id, _) in leftIdSimilarityScores)
