19e7978744a2b27521ae5e98bc006f6ead6f9c30
leonardvdj <leonardvdj@gmail.com>
2024-12-04 13:13:50 +0100

    Day 04

---
+++ Day04a/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Day04a;
+
+public static class Program
+{
+    public static void Main()
+    {
+        var input = File.ReadAllLines("input.txt");
+        const string target = "XMAS";
+
+        var count = 0;
+
+        for (var y = 0; y < input.Length; y++)
+        for (var x = 0; x < input[y].Length; x++)
+        {
+            var width = input[y].Length;
+
+            var letter = input[y][x];
+            if (letter != target[0]) continue;
+
+            for (var dirX = -1; dirX <= 1; dirX++)
+            for (var dirY = -1; dirY <= 1; dirY++)
+            {
+                if (dirX == 0 && dirY == 0) continue;
+                for (var step = 0; step < target.Length; step++)
+                {
+                    var nextX = x + dirX * step;
+                    var nextY = y + dirY * step;
+                    if (nextX < 0 || nextX >= width || nextY < 0 || nextY >= input.Length) break;
+                    var next = input[nextY][nextX];
+                    if (next != target[step]) break;
+                    if (step != 3) continue;
+                    count++;
+                    break;
+                }
+            }
+        }
+
+        Console.WriteLine(count);
+    }
+}
---
+++ Day04b/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Day04b;
+
+public static class Program
+{
+    public static void Main()
+    {
+        var input = File.ReadAllLines("input.txt");
+
+        var count = 0;
+
+        for (var y = 0; y < input.Length; y++)
+        for (var x = 0; x < input[y].Length; x++)
+        {
+            var width = input[y].Length;
+
+            if (x < 1 || x >= width - 1 || y < 1 || y >= input.Length - 1) continue;
+            var letter = input[y][x];
+            if (letter != 'A') continue;
+
+            var diag1 = Enumerable.Range(0, 3).Select(i => input[y - 1 + i][x - 1 + i]).ToList();
+            var diag1Correct = diag1.Contains('M') && diag1.Contains('S');
+            var diag2 = Enumerable.Range(0, 3).Select(i => input[y + 1 - i][x - 1 + i]).ToList();
+            var diag2Correct = diag2.Contains('M') && diag2.Contains('S');
+
+            if (diag1Correct && diag2Correct) count++;
+        }
+
+        Console.WriteLine(count);
+    }
+}
